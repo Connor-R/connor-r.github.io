d536839ffefe5418960afb6db2dd842865c9c7dd
Connor Reed <connor.reed.92@gmail.com>
2018-07-02 13:15:02 +0200

    update media files

--- media/media_run.sh
+++ media/media_run.sh
@@ -3,11 +3,19 @@ source "/Users/connordog/.bash_profile"
 
 python bookmark_parser.py
 
+wait 
+
+python tv_show_counter.py
+
+wait
+
+python tv_show_processing.py
+
 wait
 
 python export_media.py
 
-waite
+wait
 
 csvtotable /Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/csvs/relevant_bookmarks.csv -o -c "Connor Reed - Bookmarks" /Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/Tables/cr_bookmarks.html -vs 15
 python /Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/Tables/google_analytics_appender.py --file_path "/Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/Tables/cr_bookmarks.html"
@@ -21,6 +29,10 @@ python /Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/Tab
 csvtotable /Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/csvs/podcasts.csv -o -c "Connor Reed - Podcasts" /Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/Tables/podcasts.html -vs 15
 python /Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/Tables/google_analytics_appender.py --file_path "/Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/Tables/podcasts.html"
 
+
+csvtotable /Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/csvs/tvShows.csv -o -c "Connor Reed - TV Shows" /Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/Tables/tvShows.html -vs 15
+python /Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/Tables/google_analytics_appender.py --file_path "/Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/Tables/tvShows.html"
+
 cd ~/Dropbox/Desktop_Files/Work_Things/connor-r.github.io
 git add Tables/cr_bookmarks.html
 git add csvs/relevant_bookmarks.csv
@@ -30,5 +42,7 @@ git add Tables/movies.html
 git add csvs/movies.csv
 git add Tables/podcasts.html
 git add csvs/podcasts.csv
+git add Tables/tvShows.html
+git add csvs/tvShows.csv
 git commit -m "update media"
 git push
