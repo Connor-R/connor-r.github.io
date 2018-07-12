40e28826dc80fe607351c158b454e91ea0d58abf
Connor Reed <connor.reed.92@gmail.com>
2018-07-12 15:13:05 +0200

    yellow aloe update

--- climbing/8a_parser.py
+++ climbing/8a_parser.py
@@ -3,124 +3,193 @@ import time
 from datetime import datetime
 import csv
 import os
+from py_db import db
+import argparse
+import ast
+
+db = db('personal')
+
 
 base_path = os.getcwd()
 
-f = open("/Users/connordog/Dropbox/__TempFiles/Andy the Eagle God - Boulder Log-book 8a.nu.html","r")
-soup = BeautifulSoup(f.read(), "html.parser")
-f.close()
-
-csv_path = "/Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/csvs/boulders_completed.csv"
-csv_file = open(csv_path, "wb")
-append_csv = csv.writer(csv_file)
-csv_header = ["Date", "Boulder Name", "Area", "Sub Area", "Grade", "Euro Grade", "Flash", "Soft/Hard", "Stars (1-3)", "FA", "Recommended", "Attempts", "Duration", "Session #", "Comment"]
-append_csv.writerow(csv_header)
-
-ascent_data = []
-
 grade_dict = {
-    "8C+":"V16", "8C":"V15", "8B+":"V14", "8B":"V13", "8A+":"V12", "8A":"V11",
-    "7C+":"V10", "7C":"V9", "7B+":"V8", "7B":"V7/8", "7A+":"V7", "7A":"V6",
-    "6C+":"V5/6", "6C":"V5", "6B+":"V4/5", "6B":"V4", "6A+":"V3/4", "6A":"V3",
-    "5C":"V2", "5B":"V1", "5A":"V0", 
-    "4C":"V0-", "4B":"V0-", "4A":"V0-", "3C":"V0-", "3B":"V0-", "3A":"V0-", "2":"V0-"}
+    "8C+":"16", "8C":"15", "8B+":"14", "8B":"13", "8A+":"12", "8A":"11",
+    "7C+":"10", "7C":"9", "7B+":"8", "7B":"7.5", "7A+":"7", "7A":"6",
+    "6C+":"5.5", "6C":"5", "6B+":"4.5", "6B":"4", "6A+":"3.5", "6A":"3",
+    "5C":"2", "5B":"1", "5A":"0", 
+    "4C":"0", "4B":"0", "4A":"0", "3C":"0", "3B":"0", "3A":"0", "2":"0"}
 
-for comment in soup.find_all(string=lambda text:isinstance(text, Comment)):
-    if comment.strip() == "Ascents":
-        next_node = comment.next_sibling
+def initiate():
+    process_8a()
 
-        while next_node and next_node.next_sibling:
-            ascent_data.append(next_node)
-            next_node = next_node.next_sibling
+    process_completed()
+    process_undone()
 
-            if not next_node.name and next_node.strip() == "List Options": break;
+def process_8a():
+    db.query("Truncate boulders_completed;")
+    db.conn.commit()
 
-for item in ascent_data:
-    if str(item).strip() != "":
-        ascents_info = item
-        break
+    f = open("/Users/connordog/Dropbox/__TempFiles/Andy the Eagle God - Boulder Log-book 8a.nu.html","r")
+    soup = BeautifulSoup(f.read(), "html.parser")
+    f.close()
 
-ascents = ascents_info.find_all("tr")
+    ascent_data = []
 
-for i, ascent in enumerate(ascents):
-    row = []
+    for comment in soup.find_all(string=lambda text:isinstance(text, Comment)):
+        if comment.strip() == "Ascents":
+            next_node = comment.next_sibling
 
-    ascent_cells = ascent.findAll("td")
+            while next_node and next_node.next_sibling:
+                ascent_data.append(next_node)
+                next_node = next_node.next_sibling
 
-    if len(ascent_cells) == 12:
-        boulder_name = ascent_cells[7]
-        areas = ascent_cells[8].getText()
-        ascent_date = ascent_cells[4]
-        grade = ascent_cells[5]
-        flash = ascent_cells[6]
-        soft_hard_fa = ascent_cells[9].getText()
-        stars = ascent_cells[11].getText()
-        recommended = ascent_cells[3]
-        comment = ascent_cells[10]
+                if not next_node.name and next_node.strip() == "List Options": break;
 
-        for span_tag in boulder_name.find("span"):
-            span_tag.replace_with("")
-        boulder_name = boulder_name.getText().strip()
-        if boulder_name[0] == "*":
-            boulder_name = boulder_name[1:]
+    for item in ascent_data:
+        if str(item).strip() != "":
+            ascents_info = item
+            break
 
-        # print str(i-1) + " of " + str(len(ascents)-2) + ": " + boulder_name
+    ascents = ascents_info.find_all("tr")
 
-        try:
-            area = areas.split("/")[0].strip()
-            sub_area = areas.split("/")[1].strip()
-        except IndexError:
-            area = areas.strip()
-            sub_area = areas.strip()
+    for i, ascent in enumerate(ascents):
+        row = []
 
-        for span_tag in ascent_date.find("span"):
-            span_tag.replace_with("")
-        ascent_date = ascent_date.getText().strip()
-        ascent_date = datetime.strptime(ascent_date, "%y-%m-%d").date()
+        ascent_cells = ascent.findAll("td")
 
-        for script_tag in grade.find("script"):
-            script_tag.replace_with("")
-        euro_grade = grade.getText().strip()
-        grade = grade_dict.get(euro_grade)
+        if len(ascent_cells) == 12:
+            entry = {}
+            boulder_name = ascent_cells[7]
+            areas = ascent_cells[8].getText()
+            ascent_date = ascent_cells[4]
+            grade = ascent_cells[5]
+            flash = ascent_cells[6]
+            soft_hard_fa = ascent_cells[9].getText()
+            stars = len(ascent_cells[11].getText())
+            recommended = ascent_cells[3]
+            comment = ascent_cells[10]
 
-        if flash.find("img")["src"] == "Andy%20the%20Eagle%20God%20-%20Boulder%20Log-book%208a.nu_files/979607b133a6622a1fc3443e564d9577.gif":
-            flash = None
-        else:
-            flash = True
+            for span_tag in boulder_name.find("span"):
+                span_tag.replace_with("")
+            boulder_name = boulder_name.getText().strip()
+            if boulder_name[0] == "*":
+                boulder_name = boulder_name[1:]
 
-        if "Soft" in soft_hard_fa:
-            soft_hard = "soft"
-        elif "Hard" in soft_hard_fa:
-            soft_hard = "hard"
-        else:
-            soft_hard = ""
+            # print str(i-1) + " of " + str(len(ascents)-2) + ": " + boulder_name
 
-        if "FA" in soft_hard_fa:
-            fa = True
-        else:
-            fa = None
+            try:
+                area = areas.split("/")[0].strip()
+                sub_area = areas.split("/")[1].strip()
+            except IndexError:
+                area = areas.strip()
+                sub_area = areas.strip()
 
-        if recommended.find("img")["src"] == "Andy%20the%20Eagle%20God%20-%20Boulder%20Log-book%208a.nu_files/UserRecommended_0.gif":
-            recommended = None
-        else:
-            recommended = True
+            for span_tag in ascent_date.find("span"):
+                span_tag.replace_with("")
+            ascent_date = ascent_date.getText().strip()
+            ascent_date = datetime.strptime(ascent_date, "%y-%m-%d").date()
 
-        for span_tag in comment.find("span"):
-            span_tag.replace_with("")
-        comment = '"' + comment.getText().strip().replace("\n", "") + '"'
+            for script_tag in grade.find("script"):
+                script_tag.replace_with("")
+            euro_grade = grade.getText().strip()
+            v_grade = float(grade_dict.get(euro_grade))
 
-        attempts = None
-        duration = None
-        session = None
+            if flash.find("img")["src"] == "Andy%20the%20Eagle%20God%20-%20Boulder%20Log-book%208a.nu_files/979607b133a6622a1fc3443e564d9577.gif":
+                flash = None
+            else:
+                flash = "TRUE"
 
-        row = [ascent_date, boulder_name, area, sub_area, grade, euro_grade, flash, soft_hard, stars, fa, recommended, attempts, duration, session, comment]
+            if "Soft" in soft_hard_fa:
+                soft_hard = "soft"
+            elif "Hard" in soft_hard_fa:
+                soft_hard = "hard"
+            else:
+                soft_hard = ""
 
-        for j, item in enumerate(row):
-            if type(item) in (str, unicode):
-                row[j] = "".join([k if ord(k) < 128 else "" for k in item])
+            if "FA" in soft_hard_fa:
+                fa = "TRUE"
+            else:
+                fa = None
+
+            if recommended.find("img")["src"] == "Andy%20the%20Eagle%20God%20-%20Boulder%20Log-book%208a.nu_files/UserRecommended_0.gif":
+                recommended = None
+            else:
+                recommended = "TRUE"
+
+            for span_tag in comment.find("span"):
+                span_tag.replace_with("")
+            comment = comment.getText().strip().replace("\n", "")
+
+            if "Total_Duration" in comment:
+                duration_dict = comment.split("Total_Duration")[1].split("}")[0].replace("=","").strip()+"}"
+                duration_dict = ast.literal_eval(duration_dict)
+                attempts = duration_dict.get('Attempts')
+                minutes = duration_dict.get('Minutes')
+                session = duration_dict.get('Sessions')
+            else:
+                attempts = None
+                minutes = None
+                session = None
+
+            entry_columns = ["ascent_date", "boulder_name", "area", "sub_area", "v_grade", "euro_grade", "flash", "soft_hard", "stars", "fa", "recommended", "est_attempts", "est_minutes", "est_sessions", "comment"]
+            row = [ascent_date, boulder_name, area, sub_area, v_grade, euro_grade, flash, soft_hard, stars, fa, recommended, attempts, minutes, session, comment]
+
+            for j, item in enumerate(row):
+                if type(item) in (str, unicode) and item != '':
+                    row[j] = "".join([k if ord(k) < 128 else "" for k in item])
+
+            entry = {} 
+            for i,j in zip(entry_columns, row):
+                entry[i] = j
+
+            db.insertRowDict(entry, 'boulders_completed', insertMany=False, replace=True, rid=0, debug=1)
+            db.conn.commit()
+
+
+def process_completed():
+    csv_path = "/Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/csvs/boulders_completed.csv"
+    csv_file = open(csv_path, "wb")
+    append_csv = csv.writer(csv_file)
+    csv_header = ["Date", "Boulder Name", "Area", "Sub Area", "V Grade", "Euro Grade", "Flash", "Soft/Hard", "Stars (1-3)", "FA", "Recommended", "Estimated Attempts", "Estimated Minutes", "Estimated Session #", "Comment"]
+    append_csv.writerow(csv_header)
+
+    qry = "SELECT * FROM boulders_completed ORDER BY ascent_date DESC;"
+
+    res = db.query(qry)
+
+    for row in res:
+        row = list(row)
+        for i, val in enumerate(row):
+            if type(val) in (str,unicode):
+                row[i] = '"' + "".join([l if ord(l) < 128 else "" for l in val]).replace("<o>","").replace("<P>","").replace("\n","  ") + '"'
+        append_csv.writerow(row)
         
+
+def process_undone():
+    csv_path = "/Users/connordog/Dropbox/Desktop_Files/Work_Things/connor-r.github.io/csvs/boulders_undone.csv"
+    csv_file = open(csv_path, "wb")
+    append_csv = csv.writer(csv_file)
+    csv_header = ["Estimated Date", "Estimated Time", "Boulder Name", "Area", "Sub Area", "V Grade", "Estimated Attempts", "Estimated Minutes", "Comment"]
+    append_csv.writerow(csv_header)
+    
+    qry = """SELECT 
+    bt.est_date, bt.est_time,
+    bt.boulder_name, bt.area, bt.sub_area,
+    bt.v_grade, bt.est_attempts, bt.est_minutes, bt.comment
+    FROM boulders_tried bt
+    LEFT JOIN boulders_completed bc USING (boulder_name, area)
+    WHERE bc.ascent_date IS NULL
+    ORDER BY est_date DESC;"""
+
+    res = db.query(qry)
+
+    for row in res:
+        row = list(row)
+        for i, val in enumerate(row):
+            if type(val) in (str,unicode):
+                row[i] = '"' + "".join([l if ord(l) < 128 else "" for l in val]).replace("<o>","").replace("<P>","").replace("\n","  ") + '"'
         append_csv.writerow(row)
 
 
-
-
+if __name__ == "__main__":     
+    initiate()
