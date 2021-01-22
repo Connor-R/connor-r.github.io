890bf871dff0859641c46125e9a272c029bf4fcd
connor-r <connor.reed.92@gmail.com>
2021-01-22 13:26:52 -0800

    fixed ' issues

--- climbing/input_attempt.py
+++ climbing/input_attempt.py
@@ -6,42 +6,42 @@ from datetime import datetime, timedelta, date
 
 from py_db import db
 
-db = db('personal')
+db = db("personal")
 
 prompt_dict = {
-    1:['boulder_name','Boulder Name'],
-    2:['area','Area'],
-    3:['sub_area','Sub Area'],
-    4:['v_grade','V Grade'],
-    5:['session_attempts','Estimated Number of Attempts'],
-    6:['session_minutes','Estimated Number of Minutes'],
-    7:['comment','Comment'],
-    8:['session_date','Days since attempt (0 for today, 1 for yesterday, etc.)'],
-    9:['session_start','Attempted Time (24:60 format)'],
-    10:['completed', 'Completed (y/n)']
+    1:["boulder_name","Boulder Name"],
+    2:["area","Area"],
+    3:["sub_area","Sub Area"],
+    4:["v_grade","V Grade"],
+    5:["session_attempts","Estimated Number of Attempts"],
+    6:["session_minutes","Estimated Number of Minutes"],
+    7:["comment","Comment"],
+    8:["session_date","Days since attempt (0 for today, 1 for yesterday, etc.)"],
+    9:["session_start","Attempted Time (24:60 format)"],
+    10:["completed", "Completed (y/n)"]
 }
 
 completed_dict = {
-    1:['soft_hard', '\tWas the problem bounty extra soft, soft, or hard (hit RETURN for neither)'],
-    2:['stars', '\tStars (20-80)'],
-    3:['fa', '\tDid you FA this problem? (y/n)'],
-    4:['athletic', '\t\tathletic (y/n)'],
-    5:['cruxy', '\t\tcruxy (y/n)'],
-    6:['slopey', '\t\tslopey (y/n)'],
-    7:['crimpy', '\t\tcrimpy (y/n)'],
-    8:['sharp', '\t\tsharp (y/n)'],
-    9:['technical', '\t\ttechnical (y/n)'],
-    10:['power', '\t\tpower (y/n)'],
-    11:['endurance', '\t\tendurance (y/n)'],
-    12:['overhang', '\t\toverhang (y/n)'],
-    13:['vertical', '\t\tvertical (y/n)'],
-    14:['slab', '\t\tslab (y/n)'],
-    15:['roof', '\t\troof (y/n)'],
-    16:['scary', '\t\tscary (y/n)'],
+    1:["soft_hard", "\tWas the problem bounty extra soft, soft, or hard (hit RETURN for neither)"],
+    2:["stars", "\tStars (20-80)"],
+    3:["fa", "\tDid you FA this problem? (y/n)"],
+    4:["athletic", "\t\tathletic (y/n)"],
+    5:["cruxy", "\t\tcruxy (y/n)"],
+    6:["slopey", "\t\tslopey (y/n)"],
+    7:["crimpy", "\t\tcrimpy (y/n)"],
+    8:["sharp", "\t\tsharp (y/n)"],
+    9:["technical", "\t\ttechnical (y/n)"],
+    10:["power", "\t\tpower (y/n)"],
+    11:["endurance", "\t\tendurance (y/n)"],
+    12:["overhang", "\t\toverhang (y/n)"],
+    13:["vertical", "\t\tvertical (y/n)"],
+    14:["slab", "\t\tslab (y/n)"],
+    15:["roof", "\t\troof (y/n)"],
+    16:["scary", "\t\tscary (y/n)"],
 }
 
 incompleted_dict = {
-    1:['return_interest', '\tReturn Interest (0-5)'],
+    1:["return_interest", "\tReturn Interest (0-5)"],
 }
 
 def initiate():
@@ -66,16 +66,16 @@ def initiate():
         
     last_name, last_area, last_sub, last_grade, last_attempts, last_minutes, last_date, last_time, completed = last_entry
 
-    entry['boulder_name'] = last_name
-    entry['area'] = last_area
-    entry['sub_area'] = last_sub
-    entry['v_grade'] = last_grade
-    entry['session_attempts'] = last_attempts
-    entry['session_minutes'] = last_minutes
-    entry['comment'] = ""
-    entry['session_date'] = last_date
-    entry['session_start'] = last_time
-    entry['completed'] = completed
+    entry["boulder_name"] = last_name
+    entry["area"] = last_area
+    entry["sub_area"] = last_sub
+    entry["v_grade"] = last_grade
+    entry["session_attempts"] = last_attempts
+    entry["session_minutes"] = last_minutes
+    entry["comment"] = ""
+    entry["session_date"] = last_date
+    entry["session_start"] = last_time
+    entry["completed"] = completed
 
 
 
@@ -88,7 +88,7 @@ def initiate():
 
     print "\n"
 
-    if entry['completed'] == 'COMPLETED':
+    if entry["completed"] == "COMPLETED":
         j = 1
         while j < 17:
             j, cats, entry_vals = process_complete(j, entry)
@@ -108,7 +108,7 @@ def initiate():
     print "\n"
 
     # raw_input(entry)
-    db.insertRowDict(entry, 'boulder_problems', insertMany=False, replace=True, rid=0, debug=1)
+    db.insertRowDict(entry, "boulder_problems", insertMany=False, replace=True, rid=0, debug=1)
     db.conn.commit()
 
 def process_basic(i, entry):
@@ -116,49 +116,49 @@ def process_basic(i, entry):
     last_val = entry.get(cat)
     last_str = last_val
 
-    if cat == 'sub_area':
+    if cat == "sub_area":
         sub_areas = {}
 
         sa_qry = """SELECT bp.sub_area
         FROM boulder_problems bp
-        WHERE bp.area = '%s'
+        WHERE bp.area = "%s"
         GROUP BY bp.sub_area
-        ORDER BY SUM(IF(bp.completed='COMPLETED',1,0)) DESC, COUNT(*) DESC
-        ;""" % (entry.get('area'))
+        ORDER BY SUM(IF(bp.completed="COMPLETED",1,0)) DESC, COUNT(*) DESC
+        ;""" % (entry.get("area"))
 
         area_lists = db.query(sa_qry)
         if area_lists != ():
-            last_str = ''
+            last_str = ""
             # raw_input(area_lists)
             for j, sa in enumerate(area_lists):
                 sub_areas[j] = sa[0]
                 # raw_input(sub_areas)
             for k, v in sub_areas.items():
-                last_str += '\n\t' + str(k) + ' ' + str(v)
-            last_str += '\nCorresponding sub area #: '
+                last_str += "\n\t" + str(k) + " " + str(v)
+            last_str += "\nCorresponding sub area #: "
         else:
-            last_str = '(new area!): '
-    elif cat == 'comment':
-        last_str = ': '
-    elif cat == 'v_grade':
+            last_str = "(new area!): "
+    elif cat == "comment":
+        last_str = ": "
+    elif cat == "v_grade":
 
         v_qry = """SELECT DISTINCT bp.v_grade
         FROM boulder_problems bp
         WHERE 1
-            AND bp.boulder_name = '%s'
-            AND bp.area = '%s'
-            AND bp.sub_area = '%s'
-        ;""" % (entry.get('boulder_name'), entry.get('area'), entry.get('sub_area'))
+            AND bp.boulder_name = "%s"
+            AND bp.area = "%s"
+            AND bp.sub_area = "%s"
+        ;""" % (entry.get("boulder_name"), entry.get("area"), entry.get("sub_area"))
 
         prev_v = db.query(v_qry)
 
         if prev_v != () and len(prev_v) == 1:
             last_val = prev_v[0][0]
-            last_str = ' (has been recorded as %s previously): ' % (last_val)
+            last_str = " (has been recorded as %s previously): " % (last_val)
         else:
-            last_str = ' (last: ' + str(last_str) + '): '
+            last_str = " (last: " + str(last_str) + "): "
     else:
-        last_str = ' (last: ' + str(last_str) + '): '
+        last_str = " (last: " + str(last_str) + "): "
 
     user_prompt = "%s %s%s" % (i, prompt, last_str)
 
@@ -177,25 +177,25 @@ def process_basic(i, entry):
     except TypeError:
         pass
 
-    if cat == 'sub_area' and val.isdigit() and int(val) in sub_areas:
+    if cat == "sub_area" and val.isdigit() and int(val) in sub_areas:
         val = sub_areas.get(int(val))
 
-    if cat == 'session_date' and val != '':
+    if cat == "session_date" and val != "":
         cur_date = date.today()
         session_date = cur_date - timedelta(days=int(val))
         val = session_date
 
-    if cat == 'session_start':
+    if cat == "session_start":
         try:
             _time = str(int(val)) + ":00"
         except ValueError:
             _time = val
-        session_start = datetime.strptime(_time,'%H:%M').time()
+        session_start = datetime.strptime(_time,"%H:%M").time()
         val = session_start
 
-    if cat == 'completed':
-        if val.lower() == 'y':
-            val = 'COMPLETED'
+    if cat == "completed":
+        if val.lower() == "y":
+            val = "COMPLETED"
         else:
             val = None
 
@@ -219,16 +219,16 @@ def process_complete(j, entry):
     except TypeError:
         pass
 
-    if (val.lower() == "none" or val[0].lower() == 'n'):
+    if (val.lower() == "none" or val[0].lower() == "n"):
         val = None
-    elif (val[0].lower() == 'y' and cat not in ('stars', 'soft_hard')):
+    elif (val[0].lower() == "y" and cat not in ("stars", "soft_hard")):
         val = cat.upper()
-    elif (cat == 'soft_hard' and val.lower() in ('bounty extra soft', 'soft', 'hard')):
+    elif (cat == "soft_hard" and val.lower() in ("bounty extra soft", "soft", "hard")):
         val = val.upper()
-    elif (cat == 'stars' and (val > 80 or val < 20)):
+    elif (cat == "stars" and (val > 80 or val < 20)):
         val = val
     else:
-        print '\t\t\t~ERROR~ the answer %s is not a valid value for the field %s, please try again' % (val, cat)
+        print "\t\t\t~ERROR~ the answer %s is not a valid value for the field %s, please try again" % (val, cat)
         return j-1, [None], [None]
 
     return j, [cat], [val]
@@ -254,13 +254,13 @@ def process_incomplete(j, entry):
     try:
         val = int(val)
     except ValueError:
-        print '\t\t\t~ERROR~ the answer %s is not a valid value for the field %s, please try again' % (val, cat)
+        print "\t\t\t~ERROR~ the answer %s is not a valid value for the field %s, please try again" % (val, cat)
         return j-1, [None], [None]
 
-    if (cat == 'return_interest' and val <= 5 and val >= 1):
+    if (cat == "return_interest" and val <= 5 and val >= 1):
         val = val
     else:
-        print '\t\t\t~ERROR~ the answer %s is not a valid value for the field %s, please try again' % (val, cat)
+        print "\t\t\t~ERROR~ the answer %s is not a valid value for the field %s, please try again" % (val, cat)
         return j-1, [None], [None]
 
     return j, [cat], [val]
