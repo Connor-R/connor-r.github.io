21f15fedfa864cb7e95f04a4b1bbe5bfc4b34272
Connor Reed <connor.reed.92@gmail.com>
2022-01-14 03:30:01 -0800

    connor-r.github.io daily update (Jan 14, 2022)

--- quotes/quote_emailer_run.sh
+++ quotes/quote_emailer_run.sh
@@ -2,4 +2,4 @@ SHELL=/bin/bash
 source "/Users/connordog/.bash_profile"
 
 
-python quote_emailer.py --author '' --topic '' --medium '' --keyword '' --count 5 --to_address 'connor.reed.92@gmail.com'
+python quote_emailer.py --author '' --topic '' --medium '' --keyword '' --count 1 --to_address 'connor.reed.92@gmail.com'
