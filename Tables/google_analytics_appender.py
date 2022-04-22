70e04f2a2c8ac2cdefda07732361fb4f83fa017f
Connor Reed <connor.reed.92@gmail.com>
2022-04-22 10:55:21 -0700

    update climbing dataset

--- Tables/google_analytics_appender.py
+++ Tables/google_analytics_appender.py
@@ -12,6 +12,17 @@ script = """
     ga('send', 'pageview');
 """
 
+comment = "\n//<!-- Global site tag (gtag.js) - Google Analytics -->\n"
+script1 = """\n<script async src="https://www.googletagmanager.com/gtag/js?id=G-8N02F87WCP"></script>"""
+script2 = """\n
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-8N02F87WCP');
+"""
+
+
 
 # method from https://stackoverflow.com/questions/35355225/edit-and-create-html-file-using-python
 # and also from https://stackoverflow.com/questions/19123245/inserting-into-a-html-file-using-python
@@ -21,12 +32,26 @@ def add_google_analytics(file_path):
         txt = in_file.read()
         soup = bs4.BeautifulSoup(txt,"lxml")
 
-        soup.append("\n")
-        new_script = soup.new_tag("script")
-        soup.append(new_script)
+        # soup.append("\n")
+        # new_script = soup.new_tag("script")
+        # soup.append(new_script)
 
-        last_script = soup.findAll("script")[-1]
-        last_script.insert(0, script)
+        # last_script = soup.findAll("script")[-1]
+        # last_script.insert(0, script)
+
+
+
+        title = soup.find('title')
+        meta1 = soup.new_tag("script")
+        meta1.string = script2
+        title.insert_after(meta1)
+
+        meta2 = soup.new_tag('script')
+        meta2['asycncr src'] = "https://www.googletagmanager.com/gtag/js?id=G-8N02F87WCP"
+        meta2.string = comment
+
+        title.insert_after(meta2)
+
 
 
 
